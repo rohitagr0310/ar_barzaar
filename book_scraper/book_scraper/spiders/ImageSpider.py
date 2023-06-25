9912310fc6f5ef11948955d7caf31954a7033254
Rohit Agrawal <110839621+rohitagr0310@users.noreply.github.com>
2023-06-25 10:34:32 +0530

    scrapy - scraped the internet for the data

---
+++ book_scraper/book_scraper/spiders/ImageSpider.py
@@ -0,0 +1,35 @@
+import os
+import scrapy
+from urllib.parse import urljoin
+
+class ImageSpider(scrapy.Spider):
+    name = "ImageSpider"
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["https://books.toscrape.com/"]
+    counter = 1  # Initialize a counter
+
+    def parse(self, response):
+        
+        for book_link in response.css("ol.row article.product_pod a"):
+            yield response.follow(book_link.attrib["href"] , callback=self.image)
+        
+        for next_page_link in response.css("ul.pager li.next a"):
+            yield response.follow(next_page_link.attrib["href"] , callback=self.parse)
+
+    def image(self, response):
+        base_url = response.url
+        image_urls = response.css('div#product_gallery img::attr(src)').getall()
+        for image_url in image_urls:
+            absolute_image_url = urljoin(base_url, image_url)
+            yield scrapy.Request(absolute_image_url, callback=self.save_image)
+
+    def save_image(self, response):
+        image_name = f'{self.counter}.jpg'  # Generate a unique image name
+        self.counter += 1  # Increment the counter for the next image
+        folder = 'images'  # Folder name to save the images
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        file_path = os.path.join(folder, image_name)
+        with open(file_path, 'wb') as f:
+            f.write(response.body)
+        self.log(f'Saved image: {file_path}')
---
+++ book_scraper/book_scraper/spiders/books.py
@@ -0,0 +1,32 @@
+import scrapy
+import pandas as pd
+
+i = 0
+
+class BooksSpider(scrapy.Spider):
+    name = "books"
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["https://books.toscrape.com"]
+
+    def parse(self, response):
+        
+        for book_link in response.css("ol.row article.product_pod a"):
+            yield response.follow(book_link.attrib["href"] , callback=self.extract_book)
+        
+        for next_page_link in response.css("ul.pager li.next a"):
+            yield response.follow(next_page_link.attrib["href"] , callback=self.parse)
+
+
+    def extract_book(self, response):
+        title = response.css("div.product_main h1::text").get()
+        price = response.css("div.product_main p::text").get()
+        description = response.css("#product_description + p::text").get()
+        table_info = response.css("table.table").get()
+        book_info = pd.read_html(table_info)[0].set_index(0).to_dict()[1]
+        book_info["title"] = title
+        book_info["categories"] = response.css("div.page_inner ul.breadcrumb li a ::text").getall()
+        book_info['price'] = price
+        book_info['description'] = description
+        return book_info
+    
+  
